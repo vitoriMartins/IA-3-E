728e8acc8570e2c60dd31e63a808bd3228dd4282
vitoriMartins <110932209+vitoriMartins@users.noreply.github.com>
2024-08-08 07:44:15 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,7 +4,15 @@ const caixaAlternativa = document.querySelector('.caixa-alternativa');
 const caixaResultado = document.querySelector('.caixa-resultado');
 const pergunta = [
     {
-        enunciado:'pergunta 1',
-        alternaiva:'Enunciado 1'
-    }
+        enunciado:'enunciado 1',
+        alternaiva:['pergunta 1','pergunta 2']
+    },
+    {
+        enunciado:'enunciado 2',
+        alternaiva:['pergunta 3','pergunta 4']
+    },
+    {
+        enunciado:'enunciado 3',
+        alternaiva:['pergunta 5','pergunta 6']
+    },
 ]
