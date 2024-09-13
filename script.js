0c94f7816d4009b6464411bd1750794d292840a5
vitoriMartins <110932209+vitoriMartins@users.noreply.github.com>
2024-09-13 12:07:29 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -23,5 +23,14 @@ let perguntaAtual;
 function mostraPergunta(){
     perguntaAtual = perguntas[posicao];
     caixaPergunta.textContent = perguntaAtual.enunciado;
-    
+    mostraAlternativas();
 }
+
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativa){
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa;
+        caixaAlternativa.appendChild(botaoAlternativas);
+    }
+}
+mostraPergunta();
